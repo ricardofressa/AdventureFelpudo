5a4ca7605003839d1303d7239128de19f021331b
Ricardo Cardoso <rick.duk@gmail.com>
2016-02-24 15:47:28 -0300

    Add Animation in Star

--- Assets/Scripts/rotateObject.cs
+++ Assets/Scripts/rotateObject.cs
@@ -10,6 +10,6 @@ public class rotateObject : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(new Vector3(0, 0 , 60)) * Time.deltaTime;
+		transform.Rotate(new Vector3(0, 80, 0) * Time.deltaTime);
 	}
 }
