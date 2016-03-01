48f0652339461aadc3c1d4a075b05b272665c768
Ricardo Cardoso <rick.duk@gmail.com>
2016-02-29 22:31:24 -0300

    Implement script MoveCamera

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class MoveCamera : MonoBehaviour {
 
+
+	Vector3 offSet;
+	public GameObject player;
+
 	// Use this for initialization
 	void Start () {
-	
+		offSet = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.position = player.transform.position + offSet;
 	}
 }
